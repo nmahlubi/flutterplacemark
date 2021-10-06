7b687bf0c54b19dd22393908ec62187a2d1cec7a
Nomahlubi <zamahlubi27koyana@gmail.com>
2021-10-06 21:19:10 +0200

    late

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,44 @@
 import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
 import 'models/weather.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Current Weather Services',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Homepage(),
+      title: 'My Weather App',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
-
+class MyHomePage extends StatefulWidget {
   @override
-  _HomepageState createState() => _HomepageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomepageState extends State<Homepage> {
-  String location = 'Null, Press Button';
+class _MyHomePageState extends State<MyHomePage> {
+  String location = '';
+  String _weather = '';
   String Address = 'search';
-  var weather = 'Null, press button';
 
-  Future<Position> _getGeoLocationPosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      await Geolocator.openLocationSettings();
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    return await Geolocator.getCurrentPosition(
+  Future<Position> getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
+  Future<Placemark> getPlacemark(double latitude, double longitude) async {
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(latitude, longitude);
+    return placemark[0];
   }
 
   Future<void> GetAddressFromLatLong(Position position) async {
@@ -72,77 +47,64 @@ class _HomepageState extends State<Homepage> {
     print(placemarks);
     Placemark place = placemarks[0];
     Address =
-        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country},';
-
+        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
     setState(() {});
   }
 
-  void getData() async {
-    Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=-26.26598478862578&lon=27.792357735138257&appid=381c7775ecbf2b68915054705c3a5195'));
-    print(response.body);
+  Future getData(double latitude, double longitude) async {
+    String api = 'http://api.openweathermap.org/data/2.5/forecast';
+    String appId = '381c7775ecbf2b68915054705c3a5195';
+
+    String url = '$api?lat=$latitude&lon=$longitude&APPID=$appId';
+
+    http.Response response =
+        await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
+
+    Map parsed = json.decode(response.body);
+
+    return parsed['list'][0]['weather'][0]['description'];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPosition().then((position) {
+      getPlacemark(position.latitude, position.longitude).then((data) {
+        getData(position.latitude, position.longitude).then((weather) {
+          print('on line 62...$_weather');
+
+          setState(() {
+            location = data.locality!;
+            _weather = weather as String;
+          });
+        });
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('weather app'),
+        title: Text('My Weather App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text(
-            //   'Coordinates Points',
-            //   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            // ),
-            // SizedBox(
-            //   height: 10,
-            // ),
-            // Text(
-            //   location,
-            //   style: TextStyle(color: Colors.black, fontSize: 16),
-            // ),
-            // SizedBox(
-            //   height: 10,
-            // ),
-            weatherBox(Weather()),
-            Text('${getData}'),
+          children: <Widget>[
             Text(
-              'ADDRESS',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              _weather,
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text('${Address}'),
+            Text(Address),
             ElevatedButton(
                 onPressed: () async {
-                  getData();
-                  Position position = await _getGeoLocationPosition();
-                  location =
-                      'Lat: ${position.latitude} , Long: ${position.longitude}';
+                  Position position = await getPosition();
                   GetAddressFromLatLong(position);
-                  weatherBox(Weather());
                 },
-                child: Text('Get Location'))
+                child: const Text('Get location'))
           ],
         ),
       ),
-    );
-  }
-
-  Widget weatherBox(Weather weather) {
-    return Column(
-      children: [
-        Text("${weather.name}"),
-        Text("${weather.country}"),
-        Text("${weather.description}"),
-        Text("${weather.temp}"),
-        Text("${weather.lat}"),
-        Text("${weather.lon}"),
-      ],
     );
   }
 }
--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,33 +1,158 @@
-class Weather {
-  var lon;
-  var lat;
-  var description;
-  var temp;
-  var humidity;
-  var country;
-  var name;
-  var timezone;
+// class Weather {
+//   var lon;
+//   var lat;
+//   var description;
+//   var temp;
+//   var humidity;
+//   var country;
+//   var name;
+//   var timezone;
+//
+//   Weather({this.name,
+//     this.country,
+//     this.description,
+//     this.humidity,
+//     this.lat,
+//     this.lon,
+//     this.temp,
+//     this.timezone});
+//
+//   factory Weather.fromJson(Map<String, dynamic> json) {
+//     return Weather(
+//       country: json['sys']['country'] ?? "",
+//       description: json['weather']['description'] ?? "",
+//       humidity: json['main']['humidity'] ?? "",
+//       temp: json['main']['temp'] ?? "",
+//       lon: json['coord']['lon'] ?? "",
+//       lat: json['coord']['lat'] ?? "",
+//       name: json['name'] ?? "",
+//       timezone: json['timezone'] ?? "",
+//     );
+//   }
+// }
 
-  Weather(
-      {this.name,
-      this.country,
-      this.description,
-      this.humidity,
-      this.lat,
-      this.lon,
-      this.temp,
-      this.timezone});
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
-  factory Weather.fromJson(Map<String, dynamic> json) {
-    return Weather(
-      country: json['sys']['country'] ?? "",
-      description: json['weather']['description'] ?? "",
-      humidity: json['main']['humidity'] ?? "",
-      temp: json['main']['temp'] ?? "",
-      lon: json['coord']['lon'] ?? "",
-      lat: json['coord']['lat'] ?? "",
-      name: json['name'] ?? "",
-      timezone: json['timezone'] ?? "",
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  String location = 'Null, Press Button';
+  String Address = 'search';
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  Future<void> GetAddressFromLatLong(Position position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    Address =
+        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Coordinates Points',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              location,
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'ADDRESS',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('${Address}'),
+            ElevatedButton(
+                onPressed: () async {
+                  Position position = await _getGeoLocationPosition();
+                  location =
+                      'Lat: ${position.latitude} , Long: ${position.longitude}';
+                  GetAddressFromLatLong(position);
+                },
+                child: Text('Get Location'))
+          ],
+        ),
+      ),
     );
   }
 }
